e4c7051c796fd95c1451cd555923750fdb4fe7a7
tracetschida <tracetschida@utexas.edu>
2017-12-01 14:04:21 -0500

    init for multi tree

---
+++ MultiwayTree.py
@@ -0,0 +1,13 @@
+#  File: MultiwayTree.py
+#  Description: determines if two multi way trees are isometric 
+#  Student's Name: Trace Tschida
+#  Student's UT EID: TRT729
+#  Course Name: CS 313E 
+#  Unique Number: 51465
+#
+#  Date Created: 12/1/2017
+#  Date Last Modified: 12/1/2017
+
+
+def main():
+    pass
