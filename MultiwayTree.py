3f6e1b4dd879bdd874ac8d5ff52fa4352cd9a8cd
Trace Tschida <tracetschida@utexas.edu>
2017-12-04 04:04:23 +0000

    Finished Building Trees from File

--- MultiwayTree.py
+++ MultiwayTree.py
@@ -35,45 +35,93 @@ class MultiwayTree():
   
     def __init__(self, pyTree):
 
-      # set the instance variables to none
-      self.data = None
-      self.children = []
-      
-      # get the first value
-      first_val = pyTree[1]
+        if pyTree != "":
+            self.data = None
+            self.children = []
 
-      self.data = first_val
-      
-      # build the python tree with lists with a stack
-      s = Stack()
-      
-      # build a tree node
-      
-      self.addLoop(pyTree, )
+            # create a new stack to the hold the parens
+            s = Stack()
+            s.push(pyTree[0])
+            del pyTree[0]
+
+            while not s.isEmpty():
+
+                # add the remaining nodes
+                self.addChildren(pyTree, self.children, s)
+
+    def addChildTree(self):
+        pass
 
     # helper to add a tree recusively      
-    def addLoop(self, pyTree, multiTree):
+    def addChildren(self, pyTree, children, s):
+
+        # while loop until hit a number        
+        # get the first charcter
+        char = pyTree[0]
+        
       
         # check to see if starting a new list
         if char == "[":
+
+            char_after = pyTree[1]
+
+            # if the next char is a closed paren
+            # means there are no more children
+            if char_after == "]":
+                
+                # remove the [
+                del pyTree[0]
+                del pyTree[1] 
+                return 
             
+            # if the char after is not a symbol them it is a child
+            elif char_after.isalnum():
+                
+                # add the data value to the class and recure
+                children.append(MultiwayTree(pyTree))
+                # s.pop()
+                # self.data = char_after
+                # del pyTree[0]
+                # self.addChildren(pyTree, children, s)
             
-        # check to see if closing
-        elif char = "]":
+            # a new tree is being made
+            elif char_after == "[":
+                
+                # create a new tree
+                del pyTree[0]
+                s.push(pyTree[0])
+                children.append(MultiwayTree(pyTree))
+
+        # if the char is a data value
+        elif char.isalnum():
+            self.data = char
+            del pyTree[0]
+            self.addChildren(pyTree, children, s)
+        
+        # if done with child
+        elif char == "]":
             s.pop()
+            del pyTree[0]
+            return
 
-        # check to see if
+        # If comma or space
         else:
-            pass
+            del pyTree[0]
+            self.addChildren(pyTree, children, s)
 
-    def preOrder(self):
-        pass
+
+    def preOrder(self): 
+        print(self.data)
+        if self.children != []:
+            for child in self.children:
+                child.preOrder()
 
     def isIsomorpicTo(self, otherTree):
         pass
 
     def insertChild(self, newTree):
         pass
+
   
 def main():
     with open("MultiwayTreeInput.txt") as f:
@@ -84,5 +132,29 @@ def main():
         # print the first line
         print(tree_line_1)
 
+        tree_list_1 = []
+        idx = 0
+        while idx < len(tree_line_1):
+            if tree_line_1[idx].isspace() or tree_line_1[idx] == ",":
+                idx += 1
+                continue
+            else:
+                ch = tree_line_1[idx]
+                if tree_line_1[idx].isalnum() and idx < len(tree_line_1):
+                    if tree_line_1[idx + 1].isalnum():
+                        ch +=  tree_line_1[idx + 1]
+                        idx += 1
+                tree_list_1.append(ch)
+                idx +=1
+        print(tree_list_1)
+        print()
+
+        # build the first multi tree
+        tree_1 = MultiwayTree(tree_list_1)
+
+        tree_1.preOrder()
+
         # read the second line and remove the endings
         tree_line_2 = f.readline().strip()
+
+main()
